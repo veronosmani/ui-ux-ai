130120fa75a75cd5e9d0c5b8fc036093f26e6929
veronosmani <veron.osmani@gmail.com>
2024-06-24 15:49:15 +0200

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import {brainwave} from '../assets';
+
 const Header = () => {
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
-      <div>
-        
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a>
+            <img src={brainwave}/>
+        </a>
       </div>
       Header
     </div>
