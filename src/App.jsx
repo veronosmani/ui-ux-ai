c669410869594ca183b30dadb38f8c2efd2ba0a0
veronosmani <veron.osmani@gmail.com>
2024-06-24 15:42:12 +0200

    modified:   src/App.jsx 	new file:   src/components/Button.jsx 	new file:   src/components/Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import ButtonGradient from './assets/svg/ButtonGradient';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
 const App = () => {
   return (
     <>
-      <h1 className='text-3xl underline font-bold'>Hello World!</h1>
+      <h1 className="text-3xl underline font-bold">Hello World!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-20">Something</Button>
+      </div>
       <ButtonGradient />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
