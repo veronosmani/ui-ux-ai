360924ac124fbdbee2fc7c10952078806cdbf94d
veronosmani <veron.osmani@gmail.com>
2024-06-24 16:01:28 +0200

    modified:   src/components/Header.jsx 	new file:   src/constants/index.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
-import {brainwave} from '../assets';
+import { brainwave } from "../assets";
+import { navigation } from '../constants';
 
 const Header = () => {
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a>
-            <img src={brainwave}/>
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={190} height={40} alt="brainwave" />
         </a>
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+                <a>
+                
+                </a>
+            ))}
+          </div>
+        </nav>
       </div>
-      Header
     </div>
   );
 };
