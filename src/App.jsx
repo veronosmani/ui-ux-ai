5858ab930ce3e9d7f59867452c934a65128ccdfb
veronosmani <veron.osmani@gmail.com>
2024-06-24 15:47:09 +0200

    modified:   src/App.jsx 	modified:   src/components/Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl underline font-bold">Hello World!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div>
+        
+      </div>
+      Header
+    </div>
+  );
+};
 
-export default Header
+export default Header;
