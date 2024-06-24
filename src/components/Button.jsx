7cc44a6cabb1cc6e468e272065247cb8f05f631b
veronosmani <veron.osmani@gmail.com>
2024-06-24 16:47:15 +0200

    modified:   src/components/Button.jsx 	modified:   src/components/Header.jsx 	new file:   src/components/design/Benefits.jsx 	new file:   src/components/design/Collaboration.jsx 	new file:   src/components/design/Header.jsx 	new file:   src/components/design/Hero.jsx 	new file:   src/components/design/Pricing.jsx 	new file:   src/components/design/Roadmap.jsx 	new file:   src/components/design/Services.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative infline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
-  const spanClasses = "relative z-10";
+  const spanClasses = 'relative z-10';
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
@@ -21,6 +21,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
     </a>
   );
 
-  return href ? renderLink : renderButton();
+  return href ? renderLink() : renderButton();
 };
+
 export default Button;
