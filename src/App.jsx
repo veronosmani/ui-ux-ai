b396d6f0a805ce4d47f76cceb898cccf758456ac
veronosmani <veron.osmani@gmail.com>
2024-06-24 15:11:22 +0200

    modified:   package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>
-    
+    <div className='flex flex-col items-center py-10'>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
     </div>
   )
 }
