9ed202709417ebffb7e739cd25bdbd2abcfd47ee
veronosmani <veron.osmani@gmail.com>
2024-06-24 16:58:01 +0200

    modified:   src/components/Header.jsx 	modified:   src/components/design/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,21 @@ import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
+import MenuSvg from '../assets/svg/MenuSvg';
+import { HamburgerMenu } from './design/Header'
+import { useState } from "react";
 
 const Header = () => {
   const pathname = useLocation();
+    const [openNavigation, setopenNavigation] = useState(true)
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'}`}>
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="brainwave" />
         </a>
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav className={`${openNavigation ? 'flex' : 'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -27,6 +31,7 @@ const Header = () => {
                 {item.title}
               </a>
             ))}
+            <HamburgerMenu />
           </div>
         </nav>
         <a
