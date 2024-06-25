b00a04d69a11ab7b6f1a90deb97bc2129a42b3bb
veronosmani <veron.osmani@gmail.com>
2024-06-25 11:40:15 +0200

    modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,9 +21,9 @@ const Hero = () => {
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilites of&nbsp;AI&nbsp;
-            Chatting with{" "}
+            Chatting with {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              Brainwave {" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -35,7 +35,7 @@ const Hero = () => {
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
             Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the opne AI chat app.
+            with Brainwave, the open AI chat app.
           </p>
           <Button href="/pricings" white>
             Get Started
