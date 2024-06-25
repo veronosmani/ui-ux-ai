cdb06f4f655a6294a812255c960e55748a2ba2d2
veronosmani <veron.osmani@gmail.com>
2024-06-25 14:55:17 +0200

    modified:   src/components/Benefits.jsx new file:   src/components/Heading.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,34 @@
+import Heading from "./Heading";
+import { benefits } from "../constants";
+import Section from "./Section";
+
 const Benefits = () => {
   return (
-    <div>Benefits</div>
-  )
-}
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainware"
+        />
 
-export default Benefits
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{item.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
