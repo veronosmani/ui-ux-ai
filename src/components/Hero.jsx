fbbf28d5854527774572ac79febf20681aa0b56a
veronosmani <veron.osmani@gmail.com>
2024-06-25 13:05:33 +0200

    modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,8 +20,8 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilites of&nbsp;AI&nbsp;
-            Chatting with {` `}
+            Explore the Possibilites <br />
+            of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave {" "}
               <img
