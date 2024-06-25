9ebcab5d52fa40c3c72049a3caa71021ae66af2e
veronosmani <veron.osmani@gmail.com>
2024-06-25 15:57:43 +0200

    modified:   src/components/Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,8 +1,10 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
-import Arrow from '../assets/svg/Arrow';
-import {GradientLight} from './design/Benefits'
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
+
 const Benefits = () => {
   return (
     <Section id="features">
@@ -31,11 +33,33 @@ const Benefits = () => {
                     height={48}
                     alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">Explore more</p>
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
                   <Arrow />
                 </div>
               </div>
+
               {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
             </div>
           ))}
         </div>
