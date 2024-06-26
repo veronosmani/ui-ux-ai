d95cc727823dbf2efd3ac9b15a93aa0cc543641e
veronosmani <veron.osmani@gmail.com>
2024-06-26 10:49:16 +0200

    modified:   src/components/Pricing.jsx 	modified:   src/components/PricingList.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,6 +1,8 @@
 import Section from "./Section";
 import { smallSphere, stars } from "../assets";
 import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from './design/Pricing'
 
 const Pricing = () => {
   return (
@@ -29,7 +31,9 @@ const Pricing = () => {
           title="Pay once, use forever"
         />
         <div className="relative">
-            
+            <PricingList />
+            <LeftLine />
+            <RightLine />
         </div>
       </div>
     </Section>
--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,7 +1,15 @@
+import { pricing } from "../constants";
+
 const PricingList = () => {
   return (
-    <div>PricingList</div>
-  )
-}
+    <div className="flex gap-[1rem] max-lg:flex-wrap">
+      {pricing.map((item) => (
+        <div>
+          <h4>{item.title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default PricingList
+export default PricingList;
