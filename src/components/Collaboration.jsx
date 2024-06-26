bfcd58026d44aeddb9b584c036becd91093aaa3c
veronosmani <veron.osmani@gmail.com>
2024-06-26 16:38:00 +0200

    modified:   src/components/Collaboration.jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -9,7 +9,7 @@ const Collaboration = () => {
     <Section crosses>
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
-          <h2 className="h2 mb-4 md:mb-8">
+          <h2 className="h2 mb-4 md:mb-8 ">
             AI Chat App for seamless Collaboration
           </h2>
           <ul className="max-w-[22rem] mb-10 md:mb-14">
