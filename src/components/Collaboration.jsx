8e0c78efdb2ad1d7b84eaabae1d10fccecbd8194
veronosmani <veron.osmani@gmail.com>
2024-06-26 09:13:56 +0200

    modified:   src/components/Collaboration.jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,7 +1,8 @@
-import { brainwave, brainwaveSymbol, check } from "../assets";
+import { brainwaveSymbol, check } from "../assets";
 import { collabApps, collabContent, collabText } from "../constants";
 import Button from "./Button";
 import Section from "./Section";
+import { LeftCurve, RightCurve } from './design/Collaboration'
 
 const Collaboration = () => {
   return (
@@ -27,11 +28,11 @@ const Collaboration = () => {
           <Button>Try it now</Button>
         </div>
         <div className="lg:ml-auto xl: w-[38rem] mt-4">
-          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto:">
+          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto:">
             {collabText}
           </p>
 
-          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+          <div className="mt-4 relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
@@ -52,7 +53,11 @@ const Collaboration = () => {
                     index * 45
                   }`}
                 >
-                  <div className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${index * 45}`}>
+                  <div
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                      index * 45
+                    }`}
+                  >
                     <img
                       className="m-auto"
                       width={app.width}
@@ -64,6 +69,8 @@ const Collaboration = () => {
                 </li>
               ))}
             </ul>
+            <LeftCurve />
+            <RightCurve />
           </div>
         </div>
       </div>
